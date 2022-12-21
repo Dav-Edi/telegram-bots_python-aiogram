4870445dca865a33de072c832d92e53ff5ae5588
DavEdi <davedi.isme@gmail.com>
2022-12-21 11:31:08 +0300

    test dataclasses, hints

--- somefile.py
+++ somefile.py
@@ -1,4 +1,20 @@
-print("This is a file from GitHub repository")
+from dataclasses import dataclass
 
 
-print("This are new local changes")
+@dataclass
+class User:
+    user_id: int
+    name: str
+    age: int
+    email: str
+
+
+def get_user_info(user: User) -> str:
+    return f'Возраст пользователя {user.name} - {user.age}, ' \
+         f'а email - {user.email}'
+
+
+user_1: User = User(42, 'Vasiliy', 23, 'vasya_pupkin@pochta.ru')
+user_2: User = User(43, "Edi", 18, "edi_pupkin@pochta.ru")
+print(get_user_info(user_1))
+print(get_user_info(user_2))
